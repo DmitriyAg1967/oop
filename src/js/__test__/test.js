4b16e0f205f8ec17d2bfa0f206e72ba6f4ff0ace
Dmitriy <aglerimov05@mail.ru>
2022-10-22 14:29:19 +0300

    Второй коммит

--- src/js/__test__/test.js
+++ src/js/__test__/test.js
@@ -1,36 +1,30 @@
-// import {Character} from '../app';
+import Character from '../app';
 
-import {bowerman2222} from '../app';
-import {bowerman1111} from '../app';
-import {bowerman} from '../app';
-import {swordsman} from '../app';
-import {magician} from '../app';
-import {daemon} from '../app';
-import {undead} from '../app';
-import {zombie} from '../app';
-
-const referencebowerman2222 = {"attack": 25, "defence": 25};
-
-const referencebowerman1111 = {"attack": 25, "defence": 25};
+import { Bowerman } from '../app';
+import { Swordsman } from '../app';
+import { Magician } from '../app';
+import { Daemon } from '../app';
+import { Undead } from '../app';
+import { Zombie } from '../app';
 
 const referencebowerman = {
   name: 'Bowerman',
   type: 'Bowerman',
-  health: 100,     
-  level: 1,        
-  attack: 25,      
-  defence: 25      
+  health: 100,
+  level: 1,
+  attack: 25,
+  defence: 25
 };
 
 const referenceswordsman = {
   name: 'Swordsman',
   type: 'Swordsman',
-  health: 100,      
+  health: 100,
   level: 1,
-  attack: 40,       
-  defence: 10       
+  attack: 40,
+  defence: 10
 }
-const referencemagician =  {
+const referencemagician = {
   name: 'Magician',
   type: 'Magician',
   health: 100,
@@ -63,36 +57,100 @@ const referencezombie = {
   defence: 10
 }
 
-test('bowerman2222test', () => {
-  console.log(bowerman2222)
-  expect(referencebowerman2222).toEqual(bowerman2222);
+test('Не верное имя', () => {
+  expect(() => new Character('Bowerman1111', 'Bowerman')).toThrow();
 });
 
-test('bowerman1111test', () => {
-  console.log(bowerman1111)
-  expect(referencebowerman1111).toEqual(bowerman1111);
+test('Не верный тип', () => {
+  expect(() => new Bowerman('Bowerman', 'Bowerman2222')).toThrow();
+});
+
+test('Не верный тип', () => {
+  expect(() => new Swordsman('Swordsman', 'Swordsman2222')).toThrow();
+});
+
+test('Не верный тип', () => {
+  expect(() => new Magician('Magician', 'Magician2222')).toThrow();
+});
+
+test('Не верный тип', () => {
+  expect(() => new Daemon('Daemon', 'Daemon2222')).toThrow();
+});
+
+test('Не верный тип', () => {
+  expect(() => new Undead('Undead', 'Undead2222')).toThrow();
+});
+
+test('Не верный тип', () => {
+  expect(() => new Zombie('Zombie', 'Zombie2222')).toThrow();
 });
 
 test('bowermantest', () => {
+  const bowerman = new Bowerman('Bowerman', 'Bowerman')
   expect(referencebowerman).toEqual(bowerman);
 });
 
 test('swordsmantest', () => {
+  const swordsman = new Swordsman('Swordsman', 'Swordsman')
   expect(referenceswordsman).toEqual(swordsman);
-});
+})
 
 test('magiciantest', () => {
+  const magician = new Magician('Magician', 'Magician')
   expect(referencemagician).toEqual(magician);
 });
 
 test('daemontest', () => {
+  const daemon = new Daemon('Daemon', 'Daemon')
   expect(referencedaemonn).toEqual(daemon);
 });
 
 test('undeadtest', () => {
+  const undead = new Undead('Undead', 'Undead')
   expect(referenceundead).toEqual(undead);
 });
 
 test('zombietest', () => {
+  const zombie = new Zombie('Zombie', 'Zombie')
   expect(referencezombie).toEqual(zombie);
+});
+
+test('Повышаем уровень персонажа', () => {
+  const description = new Character('Bowerman');
+  description.health = 50;
+  description.attack = 10;
+  description.defense = 10;
+  description.levelUp();
+  const result = {
+    name: 'Bowerman', health: 100, level: 2, attack: 12, defense: 12,
+  };
+  expect(description).toEqual(result);
+});
+
+test('нельзя повысить левел умершего', () => {
+  function test() {
+    const description = new Character('Bowerman');
+    description.health = 0;
+    return description.levelUp();
+  }
+  expect(test).toThrow('нельзя повысить левел умершего');
+});
+
+test('Расчитываем урон персонажа', () => {
+  const description = new Character('Bowerman');
+  console.log(description);
+  description.damage(100);
+  const result = {
+    name: 'Bowerman', type: undefined, health: NaN, level: 1
+  };
+  expect(description).toEqual(result);
+});
+
+test('расчет невозможен', () => {
+  function test() {
+    const description = new Character('Bowerman');
+    description.health = 0;
+    return description.damage();
+  }
+  expect(test).toThrow('расчет невозможен');
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,89 +1,94 @@
-class Character {
-    constructor(name, type, health, level, attack, defence) {
-        try { 
-            if(name.length < 2 || name.length > 10) throw "не то количество букв";
-        }
-        catch(err) {
-            console.log ("Вы ввели " + err);
-            return
-        }
-
-        try { 
-            if (type != 'Bowerman' && type != 'Swordsman' && type != 'Magician' && type != 'Daemon' && type != 'Undead' && type != 'Zombie') throw "неправильный тип игрового персонажа.";
-        }
-        catch(err) {
-            console.log ("Вы ввели " + err);
-            return
+export default class Character {
+    constructor(name, type) {
+        if (name === undefined || name.length < 2 || name.length > 10) {
+            throw new Error('Имя должно быть сткрой от 2 до 10 символов включительно');
         }
         this.name = name;
         this.type = type;
         this.health = 100;
         this.level = 1;
-        this.attack = attack;
-        this.defence = defence;
+    }
+
+    levelUp() {
+        if (this.health === 0) {
+            throw new Error('нельзя повысить левел умершего');
+        }
+        this.level += 1;
+        this.health = 100;
+        this.attack *= 1.2;
+        this.defense *= 1.2;
+    }
+
+    damage(points) {
+        if (this.health === 0) {
+            throw new Error('расчет невозможен');
+        }
+        this.health -= points * (1 - this.defense / 100);
     }
 }
 
-
-class Bowerman extends Character {
-    constructor(name, type, health, level, attack, defence) {
-        super(name, type, health, level);
+export class Bowerman extends Character {
+    constructor(name, type) {
+        if (type != 'Bowerman') {
+            throw new Error('Неправильный тип игрового персонажа.');
+        }
+        super(name, type);
         this.attack = 25;
         this.defence = 25;
     }
 }
 
-
-class Swordsman extends Character{
-    constructor (name, type, health, level, attack, defence){
-        super(name, type, health, level);
+export class Swordsman extends Character {
+    constructor(name, type) {
+        if (type != 'Swordsman') {
+            throw new Error('Неправильный тип игрового персонажа.');
+        }
+        super(name, type);
         this.attack = 40;
-        this.defence = 10;  
-    } 
+        this.defence = 10;
+    }
 }
 
-
- 
-class Magician extends Character{
-    constructor (name, type, health, level, attack, defence){
-        super(name, type, health, level);
+export class Magician extends Character {
+    constructor(name, type) {
+        if (type != 'Magician') {
+            throw new Error('Неправильный тип игрового персонажа.');
+        }
+        super(name, type);
         this.attack = 10;
-        this.defence = 40;  
-    } 
+        this.defence = 40;
+    }
 }
 
-
-class Daemon extends Character{
-    constructor (name, type, health, level, attack, defence){
-        super(name, type, health, level);
+export class Daemon extends Character {
+    constructor(name, type) {
+        if (type != 'Daemon') {
+            throw new Error('Неправильный тип игрового персонажа.');
+        }
+        super(name, type);
         this.attack = 10;
-        this.defence = 40;  
-    } 
+        this.defence = 40;
+    }
 }
 
-
-class Undead extends Character{
-    constructor (name, type, health, level, attack, defence){
-        super(name, type, health, level);
+export class Undead extends Character {
+    constructor(name, type) {
+        if (type != 'Undead') {
+            throw new Error('Неправильный тип игрового персонажа.');
+        }
+        super(name, type);
         this.attack = 25;
-        this.defence = 25;  
-    } 
+        this.defence = 25;
+    }
 }
 
-
-class Zombie extends Character{
-    constructor (name, type, health, level, attack, defence){
-        super(name, type, health, level);
+export class Zombie extends Character {
+    constructor(name, type) {
+        if (type != 'Zombie') {
+            throw new Error('Неправильный тип игрового персонажа.');
+        }
+        super(name, type);
         this.attack = 40;
-        this.defence = 10;  
-    } 
+        this.defence = 10;
+    }
 }
-
-export let bowerman1111 = new Bowerman('Bowerman1111', 'Bowerman', null, null, null, null);
-export let bowerman2222 = new Bowerman('Bowerman', 'Bowerman2222', null, null, null, null);
-export let bowerman = new Bowerman('Bowerman', 'Bowerman', null, null, null, null);
-export let swordsman = new Swordsman('Swordsman', 'Swordsman', null, null, null, null);
-export let magician = new Magician('Magician', 'Magician', null, null, null, null);
-export let daemon = new Daemon('Daemon', 'Daemon', null, null, null, null);
-export let undead = new Undead('Undead', 'Undead', null, null, null, null);
-export let zombie = new Zombie('Zombie', 'Zombie', null, null, null, null);
